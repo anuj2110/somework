a693f7ba0784034794f1504be25577eebabd3b1b
Anuj <atrehan789@gmail.com>
2019-11-24 14:42:38 +0530

    Did the work

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'shop.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget _dialogBuilder(BuildContext context, Shop shop) {
+    ThemeData localtheme = Theme.of(context);
+    return SimpleDialog(
+      contentPadding: EdgeInsets.zero,
+      children: <Widget>[
+        Image.network(
+          shop.imgurl,
+          fit: BoxFit.fill,
+          scale: 0.3,
+          width: 150,
+          height: 250,
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                shop.shopname,
+                style: localtheme.textTheme.display1,
+              ),
+              SizedBox(height: 15.0),
+              Text(
+                shop.shopdesc,
+                style: localtheme.textTheme.subhead,
+              ),
+              SizedBox(height: 25),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Wrap(
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text("BEKAAR HAI"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    RaisedButton(
+                      splashColor: Colors.amber,
+                      child: Text("LIKED THIS"),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return ListTile(
+      onTap: () => showDialog(
+          context: context,
+          builder: (context) => _dialogBuilder(context, list[index])),
+      leading: Container(
+          width: 40.0,
+          height: 40.0,
+          decoration: new BoxDecoration(
+              shape: BoxShape.circle,
+              image: new DecorationImage(
+                  fit: BoxFit.fill, image: NetworkImage(list[index].imgurl)))),
+      title: Text(list[index].shopname),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        buttonColor: Colors.deepOrange,
+        buttonTheme: ButtonThemeData(
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("HELLO"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 50.0),
+          child: Card(
+            child: Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ListView.builder(
+                  itemCount: list.length,
+                  itemBuilder: _listItemBuilder,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shop.dart
@@ -0,0 +1,81 @@
+class Shop {
+  String shopname;
+  String shopdesc;
+  String imgurl;
+  Shop({this.shopname, this.shopdesc, this.imgurl});
+}
+
+List<Shop> list = [
+  Shop(
+      shopname: "Shop1",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop2",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop3",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop4",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop5",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop6",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop7",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop8",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop9",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop10",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop11",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop12",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop13",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop14",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop15",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop16",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop17",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png"),
+  Shop(
+      shopname: "Shop18",
+      shopdesc: "A very Good shop here",
+      imgurl: "https://i.imgur.com/BoN9kdC.png")
+];
